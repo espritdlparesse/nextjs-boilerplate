9d6d00b0f4e921feb5b517a00b028e011d02d87d
espritdlparesse <dolgopolova.anastasija@gmail.com>
2026-03-12 16:29:24 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1363,7 +1363,6 @@ export default function Page() {
                     )}
                   </div>
                 )}
-                </div>
 
                 <div className="input-group">
                   <div className="input-label">название</div>
